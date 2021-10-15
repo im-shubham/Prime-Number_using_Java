12e1de476a0f4bb15ecaf68d0df8fbd5dbcb195c
Shubham Vig <shubhamgithub@outlook.com>
2021-10-15 21:18:22 +0530

    Number of Factors if Number is not a prime number.

--- src/com/java/Main.java
+++ src/com/java/Main.java
@@ -1,4 +1,4 @@
-//Prime Number using Java
+//Check whether a number is prime or not and if not then how many factors it has using Java
 
 package com.java;
 
@@ -27,17 +27,22 @@ public class Main {
 
 
         else{
-            for(int i=2;i*i<=n;i++){        //else in other cases determine whether a number is prime or not using for loop.
-                                            // if there is a factor then it must be in i<sqrt(n) so that's why loop is till sqrt(n).
+            for(int i=2;i<=n-1;i++){        //else in other cases determine whether a number is prime or not using for loop.
+                                            // if there is a factor then it must be in i<(n-1) so that's why loop is till n-1.
+
+                // you can also use loop till sqrt(n) but here we have to find number of factors also that's why I used till n-1.
+
 
                 if (n%i==0){
                     temp+=1;            //initially temp=0 but if there will be any factor found then temp value will increase.
 
                 }
             }if(temp>0){
-                System.out.println(n+" is not a Prime number.");
+                System.out.print(n+" is not a Prime number. ");
+                System.out.println("And "+n+" has  "+ (temp+2) +" Number of factors "); // including 1 and itself that's why temp+2.
             }else{
                 System.out.println(n+" is a Prime number.");
+                System.out.println("A prime number is a natural number greater than 1 that is not a product of two smaller natural numbers.");
             }
 
         }
